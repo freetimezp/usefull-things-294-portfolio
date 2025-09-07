a2f543df0d942e13ceef46676aab2f68d2a0395f
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-09-07 22:12:41 +0300

    08. usefull-things-294-portfolio. UI. Projects section. Swiper slider. Responsive.

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,22 @@ animate(chars2, {
     delay: stagger(90),
     loop: true,
 });
+
+//slider swiper
+const swiperProjects = new Swiper(".projects__swiper", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: "auto",
+    grabCursor: true,
+    speed: 600,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
